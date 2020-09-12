3a38ac6f8870b781e3db81350962f0231bda198a
TanviCherukuri <70355172+TanviCherukuri@users.noreply.github.com>
2020-09-12 10:34:15 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var bullet,wall,speed,weight,thickness;
+
+function setup() {
+  createCanvas(1600,400);
+  speed = random(223,321);
+  weight = random(30,52); 
+  thickness = random(22,83);
+  bullet = createSprite(50,200,20,10);
+  bullet.shapeColor = color(255,255,255);
+  wall = createSprite(1200,200,thickness,height/2);
+  wall.shapeColor = color(80,80,80);
+}
+
+
+function draw() {
+  background(0,0,0);
+  bullet.velocityX = speed;
+  if (bullet.collide(wall)) {
+    bullet.velocityX = 0;
+    var damage = 0.5 * weight * speed * speed/(thickness * thickness * thickness);
+    if (damage > 10) {
+      wall.shapeColor =  color(255,0,0);    
+    }
+    if (deformation <= 10) {
+      wall.shapeColor = color(0,255,0);
+    }
+  }
+  drawSprites();
+}
